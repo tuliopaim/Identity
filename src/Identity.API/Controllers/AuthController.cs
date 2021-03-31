8df152267057db8a784eaad02cf5fae617c0a8ee
tuliopaim <tutpaim@gmail.com>
2021-03-31 19:42:37 -0300

    Adiciona Main Controller com helpers de notificação / padronização de retorno

--- src/Identity.API/Controllers/AuthController.cs
+++ src/Identity.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Identity.API.Business.Interfaces;
 using Identity.API.Entities;
 using Identity.API.ViewModels;
 using Microsoft.AspNetCore.Identity;
@@ -8,22 +9,27 @@ using Microsoft.AspNetCore.Mvc;
 namespace Identity.API.Controllers
 {
     [Route("api")]
-    public class AuthController : Controller
+    public class AuthController : MainController
     {
+        private readonly INotificador _notificador;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public AuthController(SignInManager<ApplicationUser> signInManager,
-              UserManager<ApplicationUser> userManager)
+        public AuthController(
+              SignInManager<ApplicationUser> signInManager,
+              UserManager<ApplicationUser> userManager,
+              INotificador notificador) : base(notificador)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _notificador = notificador;
         }
 
         [HttpPost("registrar")]
         public async Task<IActionResult> Registrar([FromBody]RegisterUserViewModel registerUser)
         {
-            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (!ModelState.IsValid)
+                return CustomResponse(ModelState);
 
             var user = new ApplicationUser()
             {
@@ -34,33 +40,38 @@ namespace Identity.API.Controllers
 
             var result = await _userManager.CreateAsync(user);
 
-            if (!result.Succeeded)
+            if (result.Succeeded)
             {
-                return BadRequest(result.Errors.Select(e => e.Description));
+                return CustomResponse("Usuario criado com sucesso");
             }
 
-            return Ok();
+            foreach (var erro in result.Errors)
+            {
+                NotificarErro(erro.Description);
+            }
+
+            return CustomResponse(registerUser);
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginUserViewModel loginUser)
         {
-            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (!ModelState.IsValid)
+                return CustomResponse(ModelState);
 
-            var result = 
-                await _signInManager.PasswordSignInAsync(
-                    loginUser.Email,
-                    loginUser.Password,
-                    false,
-                    false);
+            var result = await _signInManager.PasswordSignInAsync(
+                loginUser.Email,
+                loginUser.Password,
+                false,
+                false);
 
-            if (!result.Succeeded)
+            if (result.Succeeded)
             {
-                return BadRequest("Email ou Senha incorreto(a).");
+                return CustomResponse();
             }
 
-            return Ok();
+            NotificarErro("Email ou Senha incorretos");
+            return CustomResponse(loginUser);
         }
-
     }
 }
