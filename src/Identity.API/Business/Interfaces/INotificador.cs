f7d7611ac25cfb838de2686b86f8bafec4e77720
tuliopaim <tutpaim@gmail.com>
2021-03-31 19:41:38 -0300

    Cria objetos de Notificação

---
+++ src/Identity.API/Business/Interfaces/INotificador.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Identity.API.Business.Notificacoes;
+
+namespace Identity.API.Business.Interfaces
+{
+    public interface INotificador
+    {
+        void AdicionarNotificacao(Notificacao notificacao);
+        List<Notificacao> ObterNotificacoes();
+        bool TemNotificacao();
+    }
+}
