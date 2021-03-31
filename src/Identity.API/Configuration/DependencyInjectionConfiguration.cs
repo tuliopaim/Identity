4de2d3c97c63968f03ccf9ad6e932a9e288642a9
tuliopaim <tutpaim@gmail.com>
2021-03-31 20:55:54 -0300

    Altera nome de classe

--- src/Identity.API/Configuration/DependencyInjectionConfiguration.cs
+++ src/Identity.API/Configuration/DependencyInjectionConfiguration.cs
@@ -1,14 +1,16 @@
 ﻿using Identity.API.Business.Interfaces;
 using Identity.API.Business.Notificacoes;
+using Identity.API.Business.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Identity.API.Configuration
 {
-    public static class ApiConfiguration
+    public static class DependencyInjectionConfiguration
     {
         public static IServiceCollection ResolveDependences(this IServiceCollection services)
         {
             services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IJwtService, JwtService>();
 
             return services;
         }
