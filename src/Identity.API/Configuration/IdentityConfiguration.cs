1f049b445f8d41fc102ded28c1c712af28db4e9d
tuliopaim <tutpaim@gmail.com>
2021-03-31 20:57:00 -0300

    Adiciona app settings e jws em configuração do identity

--- src/Identity.API/Configuration/IdentityConfiguration.cs
+++ src/Identity.API/Configuration/IdentityConfiguration.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Text;
 using Identity.API.Data;
 using Identity.API.Entities;
+using Identity.API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Identity.API.Configuration
 {
@@ -20,7 +24,42 @@ namespace Identity.API.Configuration
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+
+            services.AddJwt(appSettings);
+
             return services;
         }
+
+        public static IServiceCollection AddJwt(this IServiceCollection services,
+            AppSettings appSettings)
+        {
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
+
+            return services;
+        }
+
     }
 }
