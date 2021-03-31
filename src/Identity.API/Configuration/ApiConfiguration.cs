c9bf64caf0a3e26c3677e97d2ebfef7724f067bd
tuliopaim <tutpaim@gmail.com>
2021-03-31 19:42:22 -0300

    Cria ApiConfiguration para DI

---
+++ src/Identity.API/Configuration/ApiConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Identity.API.Business.Interfaces;
+using Identity.API.Business.Notificacoes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.API.Configuration
+{
+    public static class ApiConfiguration
+    {
+        public static IServiceCollection ResolveDependences(this IServiceCollection services)
+        {
+            services.AddScoped<INotificador, Notificador>();
+
+            return services;
+        }
+    }
+}
