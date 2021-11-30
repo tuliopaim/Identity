a81bbeb6770bf8de71f825b4dda635b9359afef4
Túlio Paim <tutpaim@gmail.com>
2021-11-30 12:07:38 -0300

    Cria classes de request para CriarPerfil

---
+++ src/Identity.Business/Requests/Perfil/CriarPerfilPermissaoRequest.cs
@@ -0,0 +1,9 @@
+﻿using Identity.Business.Enumeradores;
+
+namespace Identity.Business.Requests.Perfil;
+
+public class CriarPerfilPermissaoRequest
+{
+    public PermissaoNomeEnum Nome { get; set; }
+    public List<PermissaoValorEnum> Permissoes { get; set; }
+}
---
+++ src/Identity.Business/Requests/Perfil/CriarPerfilRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Identity.Business.Requests.Perfil;
+
+public class CriarPerfilRequest
+{
+    public string Nome { get; set; }
+    public List<CriarPerfilPermissaoRequest> Permissoes { get; set; }
+}
