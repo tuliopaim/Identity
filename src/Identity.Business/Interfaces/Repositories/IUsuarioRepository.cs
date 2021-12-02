b2f1ad5e72d70df0927cd4cadcfac40d4b58463f
Túlio Paim <tutpaim@gmail.com>
2021-12-01 22:20:13 -0300

    Consultas para usuario

--- src/Identity.Business/Interfaces/Repositories/IUsuarioRepository.cs
+++ src/Identity.Business/Interfaces/Repositories/IUsuarioRepository.cs
@@ -4,6 +4,9 @@ namespace Identity.Business.Interfaces.Repositories
 {
     public interface IUsuarioRepository : IGenericRepository<Usuario>
     {
+        Task<Usuario?> ObterUsuarioPorId(Guid usuarioId);
         Task<Usuario?> ObterUsuarioComPerfis(Guid usuarioId);
+        Task<bool> UsuarioAtivo(string email);
+        Task<bool> UsuarioAdministrador(Guid id);
     }
 }
--- src/Identity.Data/Repositories/UsuarioRepository.cs
+++ src/Identity.Data/Repositories/UsuarioRepository.cs
@@ -14,11 +14,34 @@ namespace Identity.Data.Repositories
             _context = context;
         }
 
-        public async Task<Usuario?> ObterUsuarioComPerfis(Guid usuarioId)
+        public async Task<Usuario?> ObterUsuarioPorId(Guid id)
+        {
+            return await Get().FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<bool> UsuarioAtivo(string email)
+        {
+            var usuarioIdRemovido = await GetAsNoTracking()
+                .Where(u => u.Email == email)
+                .Select(x => new { x.Removido })
+                .FirstOrDefaultAsync();
+
+            if (usuarioIdRemovido is null) return false;
+
+            return !usuarioIdRemovido.Removido;
+        }
+
+        public async Task<Usuario?> ObterUsuarioComPerfis(Guid id)
         {
             return await Get()
                 .Include(u => u.UsuarioPerfis)
-                .FirstOrDefaultAsync(u => u.Id == usuarioId);
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<bool> UsuarioAdministrador(Guid id)
+        {
+            return await GetAsNoTracking()
+                .AnyAsync(x => x.Id == id && x.UsuarioPerfis.Any(up => up.Perfil.Name == "admin"));
         }
     }
 }
